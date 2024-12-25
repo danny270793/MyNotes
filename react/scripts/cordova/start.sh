be773d59745afe3e088c960997a30019fd03be88
Danny Vaca <danny270793@gmail.com>
2024-12-25 10:16:50 -0500

    validate platform before run

--- react/scripts/cordova/start.sh
+++ react/scripts/cordova/start.sh
@@ -3,6 +3,13 @@ set -e
 PLATFORM="$1"
 CORDOVA_APP_PATH=../cordova
 
+if [ -z "${PLATFORM}" ]
+then
+  echo "specify the platform to build as argument"
+  echo "\tnpm run cordova:build [andorid|ios]"
+  exit 1
+fi
+
 cd ${CORDOVA_APP_PATH}
 if [[ "${PLATFORM}" == "ios" ]]
 then
